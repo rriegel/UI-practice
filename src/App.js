45f492e6b2ab1db1adb3677bc0e213c10d4f7c8d
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-23 16:31:36 -0400

    added components for each tab and moved global info to Home tab

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 import Wrapper from './components/Wrapper';
-import {useState, useEffect} from 'react';
+import Home from './components/Home';
+
 
 function App() {
-  const [global, setGlobal] = useState({});
   const [date, setDate] = useState("");
   useEffect(() => {
     const getGlobal = async () => {
       const url = 'https://api.covid19api.com/summary';
       const result = await axios.get(url);
-      setGlobal(result.data.Global);
       let date = new Date(result.data.Global.Date);
       date = date.toString().split(' ');
       setDate(date.slice(0, 4).join(' '));
@@ -20,36 +20,14 @@ function App() {
   }, [date]);
 
   console.log(global);
-  // create a top bar that contains the date and the title of the app
+  // menu bar that contains the date and the title of the app
   // create a sidebar with the global data
   // create a search bar in the middile to search for country data
   return (
-    <div>
+    <div className="App">
       <Wrapper date={date}/>
-      <div className="Global">
-        <header className="Global-header">
-          <h1>
-            Global
-          </h1>
-          <p>
-            New Confirmed Cases: {global.NewConfirmed}
-          </p>
-          <p>
-            New Confirmed Cases: {global.NewConfirmed}
-          </p>
-          <p>
-            Total Confirmed Cases: {global.TotalConfirmed}
-          </p>
-          <p>
-            New Deaths: {global.NewDeaths}
-          </p>
-          <p>
-            Total Deaths: {global.TotalDeaths}
-          </p>
-        </header>
-      </div>
-
-      <div className="App">
+      <Home />
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function Home() {
+
+  const [global, setGlobal] = useState({});
+  useEffect(() => {
+    const getGlobal = async () => {
+      const url = 'https://api.covid19api.com/summary';
+      const result = await axios.get(url);
+      setGlobal(result.data.Global);
+    }
+    getGlobal();
+  }, []);
+
+  return(
+    <div className="Global">
+      <header className="Global-header">
+        <h1>
+          Global
+        </h1>
+        <p>
+          New Confirmed Cases: {global.NewConfirmed}
+        </p>
+        <p>
+          New Confirmed Cases: {global.NewConfirmed}
+        </p>
+        <p>
+          Total Confirmed Cases: {global.TotalConfirmed}
+        </p>
+        <p>
+          New Deaths: {global.NewDeaths}
+        </p>
+        <p>
+          Total Deaths: {global.TotalDeaths}
+        </p>
+      </header>
+    </div>
+  )
+};
+
+export default Home;
