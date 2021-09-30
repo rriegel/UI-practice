083d6cdf5048c0fc42a18083522d7db3580698a9
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-09-29 21:02:26 -0400

    edited test to pertain to content of app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders global stats text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const globalElement = screen.getByText(/global stats today/i);
+  expect(globalElement).toBeInTheDocument();
 });
